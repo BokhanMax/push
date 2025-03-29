7a099591e459592c35dc5f54ae1d1f8d60031f78
Max Bokhan <bokhanworks@gmail.com>
2025-03-29 13:13:36 +0200

    add password length + add spec. chars

--- docs/assets/scripts.js
+++ docs/assets/scripts.js
@@ -4,8 +4,9 @@ document.getElementById('newpassbutton').addEventListener('click', function() {
 
 
 function generatePassword() {
-    var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+	const passwordLength = document.getElementById('passwordLength');
+    var length = passwordLength,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~|}{[]\:;?></-=",
         retVal = "";
     for (var i = 0, n = charset.length; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
